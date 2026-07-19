e19a3ade5f816db73eb1af1bd8a52439c0e1899d
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2026-07-19 13:49:54 -0300

    test(qemu): cover rootfs libc detection

--- app/src/test/java/com/vectras/vm/qemu/QemuExecConfigRuntimeTest.java
+++ app/src/test/java/com/vectras/vm/qemu/QemuExecConfigRuntimeTest.java
@@ -1,9 +1,16 @@
 package com.vectras.vm.qemu;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
 
 public class QemuExecConfigRuntimeTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
     @Test
     public void androidAbisMapToArtifactRuntimeArchitectures() {
@@ -17,5 +24,32 @@ public class QemuExecConfigRuntimeTest {
     public void ambiguousAbiIsRejected() {
         Assert.assertEquals("", QemuExecConfig.normalizeHostAbi("mips64"));
         Assert.assertEquals("", QemuExecConfig.normalizeHostAbi(null));
+    }
+
+    @Test
+    public void alpineMarkerSelectsMusl() throws Exception {
+        File rootfs = temporaryFolder.newFolder("alpine-rootfs");
+        File etc = new File(rootfs, "etc");
+        Assert.assertTrue(etc.mkdirs());
+        Assert.assertTrue(new File(etc, "alpine-release").createNewFile());
+
+        Assert.assertEquals("musl", QemuExecConfig.detectRootfsLibc(rootfs));
+    }
+
+    @Test
+    public void glibcLoaderSelectsGlibc() throws Exception {
+        File rootfs = temporaryFolder.newFolder("glibc-rootfs");
+        File lib = new File(rootfs, "lib");
+        Assert.assertTrue(lib.mkdirs());
+        Assert.assertTrue(new File(lib, "ld-linux-aarch64.so.1").createNewFile());
+
+        Assert.assertEquals("glibc", QemuExecConfig.detectRootfsLibc(rootfs));
+    }
+
+    @Test
+    public void unknownRootfsIsRejected() throws Exception {
+        File rootfs = temporaryFolder.newFolder("unknown-rootfs");
+        Assert.assertEquals("", QemuExecConfig.detectRootfsLibc(rootfs));
+        Assert.assertEquals("", QemuExecConfig.detectRootfsLibc(null));
     }
 }
